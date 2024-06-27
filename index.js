d7e322fa0c2c82d670a576ca8b2a126fe1869275
Seth Mwebi <sethmwebi27@gmail.com>
2024-06-27 08:21:57 +0300

    adds code to calculate distance and fare in NYC according to Scuber

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const distanceFromHqInBlocks = (location) => {
+  return Math.abs(location - 42);
+};
+
+const distanceFromHqInFeet = (location) => {
+  return Math.abs(location - 42) * 264;
+};
+
+const distanceTravelledInFeet = (start, destination) => {
+  return Math.abs(destination - start) * 264;
+};
+
+const centsToDollars = (cents) => {
+  const dollars = Math.floor(cents / 100);
+  const remainingCents = cents % 100;
+  const formattedCents = remainingCents.toString().padStart(2, "0");
+  return parseFloat(`${dollars}.${formattedCents}`);
+};
+
+const calculatesFarePrice = (start, destination) => {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return centsToDollars((distance - 400) * 2);
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else if (distance > 2500) {
+    return "cannot travel that far";
+  }
+};
